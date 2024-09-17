3e8d7751aede8eab417226c64ea79d1e4f52e98a
Arijhajjaji22 <arij.hajjaji@esprit.tn>
2024-09-17 13:09:17 +0100

    first commit

---
+++ App_plateforme_de_recrutement.Tests/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_plateforme_de_recrutement.Tests.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
